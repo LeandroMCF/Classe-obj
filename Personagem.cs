6e8fcd9935cb635c9148fba925aad60d8ace2c93
Leandro Cavalcanti <leand@DESKTOP-LFIP8ID.(none)>
2020-12-01 09:15:34 -0300

    Objeto classe

---
+++ Personagem.cs
@@ -0,0 +1,19 @@
+namespace Classe_obj
+{
+    public class Pesonagem
+    {
+        public string nome = "Tony Stark";
+
+        public int idade = 40;
+
+        public string armadura;
+
+        public string Atacar(){
+            return "Ataque de fogo!";
+        }
+
+        public string Defender(){
+            return "Não foi dessa vez!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classe_obj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pesonagem objPersonagem = new Pesonagem();
+
+            objPersonagem.nome = "Tsukamoto";
+            objPersonagem.idade = 32;
+            objPersonagem.armadura = "Voadora";
+
+            Pesonagem objPersonagem2 = new Pesonagem();
+
+            objPersonagem2.nome = "Joyce";
+            objPersonagem2.idade = 18;
+            objPersonagem2.armadura = "Flash";
+
+            Console.WriteLine($"Personagem 1 = {objPersonagem.nome}");
+            Console.WriteLine($"Personagem 2 = {objPersonagem2.nome}"); 
+            Console.WriteLine($"Ataque = {objPersonagem.Atacar()}");
+            Console.WriteLine($"Defesa = {objPersonagem2.Defender()}"); 
+        }
+    }
+}
